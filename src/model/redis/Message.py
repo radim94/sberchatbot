bfcba5fe721d6f4e3f39a9a689ac24367d6dcda1
Avtaev AS <asavt92@gmail.com>
2019-10-24 01:15:38 +0400

    add model for redis (User)

---
+++ src/model/redis/Message.py
@@ -0,0 +1,21 @@
+from walrus import *
+
+db = Database()
+
+
+class Message(Model):
+    """
+    Базовый объект сообщения
+
+    """
+
+    __database__ = db
+    __namespace__ = 'messages'
+
+    id = UUIDField(primary_key=True)
+    user_id = UUIDField(index=True)
+    chatid = TextField()
+
+    content = Field()
+
+    creation_date = DateField(index=True)
