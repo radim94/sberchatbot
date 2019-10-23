25bbf18001025e0bc36d006dab14d623b9cd1995
Avtaev AS <asavt92@gmail.com>
2019-10-23 23:20:39 +0400

    0

---
+++ src/prometheus/prometheus_logic.py
@@ -0,0 +1,18 @@
+
+
+class PrometheusService():
+    """
+    Сервис для подписок, отписок от рассылок
+
+    """
+
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def subscribe(self, filter):
+        pass
+
+
+    def insubsribe(self, ):
+        pass
---
+++ src/prometheus/prometheus_service.py
@@ -0,0 +1,18 @@
+
+
+class PrometheusService():
+    """
+    Сервис для работы с метриками, поставляемымт Прометеусом
+
+    """
+
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_metrics(self, filter):
+        pass
+
+
+    def get_alerts(self, filter):
+        pass
