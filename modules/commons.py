012e22f143b72cb5a70a42a7e53e10c844f07d0d
Your Name <you@example.com>
2019-10-25 04:58:19 +0300

    add change password

--- modules/commons.py
+++ modules/commons.py
@@ -1,3 +1,6 @@
+from text_commands import set_credentials
+
+
 def answer_menu(args, answer, credentials):
     answer.buttons = ({"value": "button_one", "label": "button_one"},
                       {"value": "button_two", "label": "button_two"})
@@ -13,3 +16,10 @@ def answer_unknown(args, answer, credentials):
 
 def answer_select(args, answer):
     answer.selects = ({'tasks TEST-8': 'TEST-8'}, "Tasks")
+
+def answer_credentials(args,answer,credentials):
+    if len(args)!=2:
+        answer.text='use credentials <login>  <password> '
+    else:
+        set_credentials(credentials['user_id'], args[0], args[1])
+        answer.text='password changed'
